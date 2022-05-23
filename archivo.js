9e7a7c5e88d0c6f0504a51234504859c796b1607
valenvg18 <valentina.villag@udea.edu.co>
2022-05-22 22:58:13 -0500

    Primera de html y javascript

---
+++ archivo.js
@@ -0,0 +1,16 @@
+function cambiarColor(color) {
+    var body = document.getElementById('body')
+    body.style.background = color.value
+}
+
+function cambiarColorLista () {
+    let coloresLista = document.getElementById("lista")
+
+    for (let i = 1; i < coloresLista.length; i++) {
+        const element = coloresLista[i]
+        if (i %2 === 0) {
+            coloresLista.element
+        }
+        
+    }
+}
