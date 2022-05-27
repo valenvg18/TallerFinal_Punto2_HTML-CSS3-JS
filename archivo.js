ab9c08e2fff17429a3c355b96765d6be3713d43b
valenvg18 <valentina.villag@udea.edu.co>
2022-05-27 13:28:11 -0500

    JS terminado para seleccionar pares e impares

--- archivo.js
+++ archivo.js
@@ -1,16 +1,47 @@
 function cambiarColor(color) {
-    var body = document.getElementById('body')
-    body.style.background = color.value
+    /*var body = document.getElementById('body')
+    body.style.background = color.value*/
+    let coloresLista = document.getElementsByTagName("li");
+    let pares = [];
+    let impares = [];
+    console.log(color.value);
+    for (let i = 0; i < coloresLista.length; i++) {
+
+        if (i % 2 !== 0) {
+            pares.push(coloresLista[i])
+        } else {
+            impares.push(coloresLista[i])
+        }
+
+        switch (color.value) {
+            case 'green':
+                for (let index = 0; index < impares.length; index++) {
+                    impares[index].style.background = 'pink';
+                }
+                for (let index = 0; index < pares.length; index++) {
+                    pares[index].style.background = color.value;
+                }
+                break;
+
+            case 'beige':
+                for (let index = 0; index < impares.length; index++) {
+                    impares[index].style.background = color.value;
+                }
+                for (let index = 0; index < pares.length; index++) {
+                    pares[index].style.background = 'pink';
+                }
+                break;
+            default:
+                for (let index = 0; index < coloresLista.length; index++) {
+                    coloresLista[index].style.background = 'pink';
+                }
+                break;
+        }
+
+
+    }
 }
 
-function cambiarColorLista () {
-    let coloresLista = document.getElementById("lista")
+function cambiarColorLista() {
 
-    for (let i = 1; i < coloresLista.length; i++) {
-        const element = coloresLista[i]
-        if (i %2 === 0) {
-            coloresLista.element
-        }
-        
-    }
 }
